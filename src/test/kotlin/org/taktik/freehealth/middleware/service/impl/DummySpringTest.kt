25872a825cb6c1c47f4c2d2db767197d8f04958e
Alexandre Neuville <alexandre.neuville@beoogo.be>
2022-06-21 10:58:43 +0200

    Fix unit tests

--- src/test/kotlin/org/taktik/freehealth/middleware/service/impl/DummySpringTest.kt
+++ src/test/kotlin/org/taktik/freehealth/middleware/service/impl/DummySpringTest.kt
@@ -6,14 +6,14 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
-import org.taktik.freehealth.middleware.service.RecipeService
+import org.taktik.freehealth.middleware.service.RecipeV4Service
 
 /**
  * @author Bernard Paulus on 14/04/17.
  */
 @RunWith(SpringRunner::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class DummySpringTest(val recipeService: RecipeService) {
+class DummySpringTest(val recipeService: RecipeV4Service) {
     @Test
     fun test() {
         assertNotNull(recipeService)
