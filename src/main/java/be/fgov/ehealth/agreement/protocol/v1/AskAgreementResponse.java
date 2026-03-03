99c635afb37dc37df67973d65c4d3c0e915fbaef
Tony Renard <tony.renard@beoogo.be>
2026-03-03 15:09:15 +0100

    Fix SendResponseType version. Attempt to fix fhir xml format
    # Conflicts:
    #	src/main/java/be/fgov/ehealth/agreement/protocol/v1/AskAgreementResponse.java
    #	src/main/kotlin/org/taktik/freehealth/middleware/service/EagreementService.kt
    #	src/main/kotlin/org/taktik/freehealth/middleware/service/impl/EagreementServiceImpl.kt
    #	src/main/kotlin/org/taktik/freehealth/middleware/web/controllers/EagreementController.kt

--- src/main/java/be/fgov/ehealth/agreement/protocol/v1/AskAgreementResponse.java
+++ src/main/java/be/fgov/ehealth/agreement/protocol/v1/AskAgreementResponse.java
@@ -2,7 +2,6 @@ package be.fgov.ehealth.agreement.protocol.v1;
 
 import be.fgov.ehealth.commons.protocol.SoapConversationLogger;
 import be.fgov.ehealth.mycarenet.commons.protocol.v3.SendResponseType;
-
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.soap.SOAPMessage;
